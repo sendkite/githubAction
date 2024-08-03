b83b845742e464304fdc908100cf199afdd017b2
sendkite <sendkite1@gmail.com>
2024-08-04 00:56:51 +0900

    chore: gitignore

--- scripts/gh_action.sh
+++ scripts/gh_action.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PROJECT_NAME="sendkite"
-JAR_PATH="/home/ubuntu/github_action/build/libs/*.jar"
+JAR_PATH="/home/ubuntu/sendkite/build/libs/*.jar"
 DEPLOY_PATH=/home/ubuntu/$PROJECT_NAME/
 DEPLOY_LOG_PATH="/home/ubuntu/$PROJECT_NAME/deploy.log"
 DEPLOY_ERR_LOG_PATH="/home/ubuntu/$PROJECT_NAME/deploy_err.log"
