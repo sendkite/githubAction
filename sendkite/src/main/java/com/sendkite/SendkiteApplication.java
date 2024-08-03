11881ab0abca1d57c9dfbc8996e843e138f2d066
sendkite <sendkite1@gmail.com>
2024-08-04 00:18:06 +0900

    project setup && appspec.yml

---
+++ sendkite/src/main/java/com/sendkite/SendkiteApplication.java
@@ -0,0 +1,13 @@
+package com.sendkite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendkiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SendkiteApplication.class, args);
+    }
+
+}
---
+++ sendkite/src/test/java/com/sendkite/SendkiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sendkite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendkiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
